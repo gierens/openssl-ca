0c8859fbf6e1162e2f07f58b47a907127b003bff
Sandro-Alessio Gierens <sandro@gierens.de>
2023-05-04 14:59:26 +0200

    Add name argument to create_csr.sh

--- create_csr.sh
+++ create_csr.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-openssl req -config server.example.com.cnf -newkey rsa:4096 -sha512 -nodes -out server.example.com.csr -outform PEM
+NAME=$1
+if [ -z "$NAME" ]; then
+    echo "Usage: $0 <name>"
+    exit 1
+fi
+openssl req -config $NAME.cnf -newkey rsa:4096 -sha512 -nodes -out $NAME.csr -outform PEM
